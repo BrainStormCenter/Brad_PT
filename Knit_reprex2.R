d052cf3496a186ef15f1a61660f2677795158dae
Jason <chejux@gmail.com>
2020-03-18 14:00:07 -0500

    Knit issues

--- Knit_reprex2.R
+++ Knit_reprex2.R
@@ -5,12 +5,10 @@ date: "3/18/2020"
 output: html_document
 ---
 
-```{r setup, include=FALSE}
+r setup, include=FALSE
 knitr::opts_chunk$set(echo = TRUE)
-```
 
 
-```{r ---> Libraries}
 library(doBy)
 library(fuzzyjoin)
 library(reshape2)
@@ -29,30 +27,38 @@ library(here)
 here()
 dr_here(show_reason = FALSE)
 #		DONE AS OF March 17, 2020
-```
 
-```{r ---> Import data}
+
+# Import data
 # Small dataset
-sm.dat <- read.csv(url('https://github.com/BrainStormCenter/Brad_PT/smData_v1.csv'), 
-				   header = TRUE)
+sm.dat <- read.csv(url('https://raw.githubusercontent.com/BrainStormCenter/Brad_PT/master/smData_v1.csv'), header = TRUE)
 
-```
 
 ##	EXAMINE DATA
-
 ###	BDI
-```{r ---> BDI Summary}
-#		BDI VARIABLES
-sm.dat.bdi <- sm.dat[c(1:2,grep("bdi", colnames(dat1)))]
-view(dat.bdi)
-
-sm.bdi.smry1  <- sm.dat %>%
+#Summary
+#		BDI VARIABLE
+sm.bdi.smry  <- sm.dat %>%
 	group_by(group.factor) %>%
 	summarise(
 		count = n(),
 		avg.bdi.tot = mean(bdi_total, na.rm = TRUE),
 		sd.bdi.tot = sd(bdi_total, na.rm = TRUE)
-		# avg.pdi.tot = mean(bdi_total, na.rm = TRUE),
-		# sd.pdi.tot = sd(pdi_total, na.rm = TRUE)
 	)
-view(bdi.smry1)
+view(sm.bdi.smry)
+#		KNIT WORKS
+
+
+
+#		COMPUTE ANOVA OF BDI
+bdi.aov = aov(bdi_total ~ group.factor, data=sm.dat)
+
+#		SUMMARY OF THE ANALYSES
+summary(bdi.aov)
+
+
+
+#		BOX PLOT
+# ggplot(data = sm.dat, mapping = aes(x = group.factor, y = bdi_total)) +
+# 	geom_boxplot()
+
